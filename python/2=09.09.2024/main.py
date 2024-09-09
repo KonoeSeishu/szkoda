478a253b67b32c4e332fe8c003e4f6a25f16b374
konoe seishu <konoeseishu@gmail.com>
2024-09-09 11:13:04 +0000

    zmienne2

---
+++ python/2=09.09.2024/main.py
@@ -0,0 +1,21 @@
+zUmu = "uwu"    #string
+print (zUmu)    
+
+zWmW = 8832     #int
+print (zWmW)
+
+# def = 1233    #blacklisted name
+
+print (1+1)
+
+print (2-1)
+
+print (2*5)
+
+print (2/1)
+
+print (4**2)
+
+text = "Hello Cat!"
+year = 2024
+print (text + " Meow!", year)
---
+++ python/2=09.09.2024/z1.py
@@ -0,0 +1,6 @@
+title = "New Email"
+content = "Random information"
+sender = "XXX"
+reciver = "YYY"
+date = "09.09.2024"
+number_at = 2
