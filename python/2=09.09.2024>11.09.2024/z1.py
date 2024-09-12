8ba4072b6bb38625fced69994ed2b04241b6894c
konoe seishu <konoeseishu@gmail.com>
2024-09-12 07:33:19 +0000

    variables

---
+++ python/2=09.09.2024>11.09.2024/z1.py
@@ -0,0 +1,32 @@
+title = "New Email"
+content = "Random information"
+sender = "XXX"
+reciver = "YYY"
+date = "09.09.2024"
+number_at = 2
+
+print("Tytul maila: " + title)      # "+" only for strings
+print("Tresc maila: ", content)
+print("Nadawca: ", sender)
+print("Odbiorca: ", reciver)
+print("Data: ", date)
+print("Ilosc zalacznikow: ", number_at)
+
+"""
+Tytul maila: New Email
+Tresc maila:  Random information
+Nadawca:  XXX
+Odbiorca:  YYY
+Data:  09.09.2024
+Ilosc zalacznikow:  2
+"""
+
+#zad 1
+print (2**64) 
+
+
+
+#zad 2
+t=int(input())
+V=int(input())
+print(V*t)
---
+++ python/3=12.09.2024/main.py
@@ -0,0 +1,51 @@
+data=24
+print(type(data))
+
+info='koty'
+print(type(info))
+
+prze=2.137
+print(type(prze))
+
+nya=20+5j-3j**2
+print(type(nya))
+
+avg1=(4+6+7+9+12)/5
+print(type(avg1))
+
+strin="kotki"
+print(type(strin))
+print(strin[3])
+
+x=strin[3]
+print(x)
+print(strin[4:])
+print(strin[::2])
+print(strin[2:8:1])
+print(strin[-2])
+print(strin*9)
+
+line="""
+UwU
+OWO
+EWE
+"""
+print(line)
+
+linenext="owo\nuwu\ttab\nnext"
+#\n next line
+#\t tab
+print(linenext)
+print(len(line)) #string count
+
+data2=input("Gimmeeee data!!!! ")
+print(data2)
+
+
+#/FREE CANDY\
+print("Gimme name: ")
+name=input()
+sname=input("Gimme sname: ")
+city= input(">>>")
+print(f"{name}{sname}{city}")
+
