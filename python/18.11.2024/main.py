67de9b45f640b2ff6bd1520d7108d0357f28f0ac
konoe seishu <konoeseishu@gmail.com>
2024-11-20 11:22:50 +0000

    python

--- python/18.11.2024/main.py
+++ python/18.11.2024/main.py
@@ -64,4 +64,54 @@ print(20 >= 20)
 print(20 <= 24)
 
 print("Logical operators")
-print(True and False) #m
+print(True and True) #T
+print(False and False) #F
+print(True and False) #F
+print(True and False) #F
+print(True and False) #T
+print(False and False) #T
+
+print(True or True) #T
+print(False or True) #T
+print(True or False) #T
+print(False or False) #F
+
+print(10 > 5 and 3==3) #T
+print(4 >= 4 and 4>=12) #Fz
+
+if 12 >= 6 and 5 > 3:
+    print(True)
+
+if "al" == "al" or 14 >= 14:
+    print(True)
+
+
+print(not True)
+print(not False)
+
+print(not(10==10)) #F
+
+#ex2
+
+age = 12
+height = 150
+
+if age >= 10 and height >= 140:
+    print("pass")
+else:
+    print("not pass")
+
+
+#ownership operators (in, not in)
+#they allow to search if value is in data container
+
+listDate1 = [1,2,3,4,5,6]
+print(0 in listDate1)
+print(4 in listDate1)
+
+print("Jan" in ("Anna", "May", "Bread"))
+
+print(12 not in listDate1)
+print(2 not in listDate1)
+
+
